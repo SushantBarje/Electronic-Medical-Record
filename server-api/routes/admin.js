d9ba863bc212498e103025b5fb85dc24b9f6bde3
Sushant Barje <sushantbarje4@gmail.com>
2022-01-15 19:01:20 +0530

    setup explorer

--- server-api/routes/admin.js
+++ server-api/routes/admin.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const FabricCAServices = require('fabric-ca-client');
+const { Wallets } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res) => {
     res.send('Hello World');
