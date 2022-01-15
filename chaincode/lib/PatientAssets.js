1a7845f28da37be0ae4d3e17ff61928c03205481
Sushant Barje <sushantbarje4@gmail.com>
2022-01-15 19:49:12 +0530

    Created patient schema class

---
+++ chaincode/lib/PatientAssets.js
@@ -0,0 +1,29 @@
+/**
+ * @author Sushant Barje
+ * @create Sat Jan 15 19:36:02 IST 2022
+ * @description Schema class of patient
+ */
+
+class PatientAsset {
+
+    constructor(patientId = '', firstName = '', middleName = '', lastName = '', password = '', age = '', phoneNumber = '', address = '', bloodGroup = '', updatedBy = '', symptoms = '', diagnosis = '', treatment = '', other = '') {
+        this.patientId = patientId;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.password = crypto.createHash('sha256').update(password).digest('hex');
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.bloodGroup = bloodGroup;
+        this.updatedBy = updatedBy;
+        this.symptoms = symptoms;
+        this.diagnosis = diagnosis;
+        this.treatment = this.treatment;
+        this.other = other;
+        this.permissionGranted = [];
+        return this;
+    }
+}
+
+module.exports = PatientAsset;
