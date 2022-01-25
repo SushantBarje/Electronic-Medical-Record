248fd1d4238d19374a92c7c7528f745e8861363b
Sushant Barje <sushantbarje4@gmail.com>
2022-01-25 19:26:16 +0530

    Created private collections

--- chaincode/lib/CommonContract.js
+++ chaincode/lib/CommonContract.js
@@ -59,6 +59,25 @@ class CommonContract extends Contract {
         let asset = JSON.parse(patient.toString());
         return asset;
     }
+
+    async getAllPatient(ctx){
+        const startKey = '';
+        const endKey = '';
+        const allResults = [];
+        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)){
+            const strValue = Buffer.from(value).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push({ Key: key, Record: record });
+        }
+        console.info(allResults);
+        return JSON.stringify(allResults);
+    }
 }
 
 module.exports = CommonContract;
