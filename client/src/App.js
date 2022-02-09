4bc3583fd420dd674ae930f0c748c415c34c1372
MUNGASE ROHIT YUVRAJ <rohitmungase1@gmail.com>
2022-02-09 15:53:34 +0530

    commit dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
-import './App.css';
-import AdminDashboard from './components/AdminDashboard/AdminDashboard';
-import Header from './components/Header/Header';
-import Login from './components/Login/Login';
-import {BrowserRouter,Routes, Route, Link} from 'react-router-dom';
 
-const pages = [];
-const settings = [];
+//import { Container,  } from '@mui/material';
+//import './App.css';
+//import Header from './components/Header';
+//import FeaturedPost from "./components/FeaturedPost";
+//import PostCard from "./components/PostCard";
+//import { featuredPosts, sidebar } from "./Data/Data";
+//import Main from "./components/Main";
+//import Sidebar from "./components/Sidebar";
+//import Footer from "./components/Footer";
+//import AdminDashboard from './components/AdminDashboard/AdminDashboard';
+//import Header from './components/Header/Header';
+//import Login from './components/Login/Login';
+//import {BrowserRouter,Routes, Route, Link} from 'react-router-dom';
+import "./App.css";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App"> 
-      <div className="app-body">
-
-      </div>
+    <div className="App">
+      <Dashboard />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ client/src/hoc/AuthRoute.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route } from "react-router";
+
+const AuthRoute = (props) => {
+  return <Route {...props} />;
+};
+
+export default AuthRoute;
