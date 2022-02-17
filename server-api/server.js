861022446fb3e77365c2b9641f837bdfa9ad47c2
Sushant Barje <sushantbarje4@gmail.com>
2022-02-17 19:59:06 +0530

    added connection to redis connection

--- server-api/server.js
+++ server-api/server.js
@@ -4,6 +4,10 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const redis = require('redis');
+const util = require('util');
+
+
 var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -25,6 +29,23 @@ app.use('/api/admin', adminRouter);
 
 const PORT = process.env.PORT || 3000;
 
+(async () => {
+    const redisClient = redis.createClient({url: 'redis://:laboratorypassword@127.0.0.1:6380'});
+    await redisClient.connect();
+    //await redisClient.AUTH('doctorpassword');
+    // redisClient.on('error', (err) => {
+    //     console.log('error');
+    // });
+    
+    const res = await redisClient.get('framework');
+    console.log(res);
+})();
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-})
+});
+
+
--- server-api/utils/utils.js
+++ server-api/utils/utils.js
@@ -3,7 +3,23 @@
 * @created 2022-02-17 T 10:14:22 
 */
 
+const redis = require('redis');
+
 exports.ADMIN_ROLE = 'admin';
-exports.DOCTOR_ROLE= 'doctor';
+exports.DOCTOR_ROLE = 'doctor';
 exports.PATIENT = 'patient';
 
+exports.createRedisConnection = async (org) => {
+
+  let url;
+  if (org === 'doctor') {
+    url = 'redis://:doctorpassword@127.0.0.1:6379';
+  } else if (org === 'laboratory') {
+    url = 'redis://:laboratorypassword@127.0.0.1:6380';
+  } else {
+    return null;
+  }
+  const redisClient = redis.createClient({ url: url });
+  redisClient.connect();
+  return redisClient;
+}
